ae22295beab107f8933e71d2ca774df570cb118d
Dimon <bolilui.dmytro@gmail.com>
2024-01-08 15:30:50 +0200

    Added user input elements and implemented two-way-bindings

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,13 @@ import { useState } from 'react';
 
 // * Import Components
 import Header from './components/Header';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <>
       <Header />
-      <div id='user-input'>
-        <div id='input-group'>
-          <label htmlFor=''>Initial Investment</label>
-          <input type='text' />
-
-          <label htmlFor=''> Annual Investment</label>
-          <input type='text' />
-
-          <label htmlFor=''>Expected Return</label>
-          <input type='text' />
-
-          <label htmlFor=''>Duration </label>
-          <input type='text' />
-        </div>
-      </div>
+      <UserInput />
     </>
   );
 }
